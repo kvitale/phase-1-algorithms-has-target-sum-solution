f229ba5d476e18d1ad47f3593949cc4734f6d970
Kyle <ktvitale7@gmail.com>
2022-06-16 20:48:51 -0400

    commit tree 8497f00cfb82c1c9770a4b137d64225895d12ee6

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+// function hasTargetSum(array, target) {
+//   for (let i = 0; i < array.length; i++) {
+//     const numMatch = (target - array[i])
+//     for (let j = i + 1; j < array.length; j++) {
+//       if (array[j] === numMatch) {
+//         return true
+//       }
+//     }
+//   }
+//   return false
+// }
+
+function hasTargetSum(array, target){
+  const seenNumbers = {}
+    for (let i = 0; i < array.length; i++){
+      const complement = target - array[i]
+      if (seenNumbers[complement]) return true
+      seenNumbers[array[i]] = true
+      
+    
+  }
+  return false
 }
+
 
 /* 
   Write the Big O time complexity of your function here
+  Run: 0(n^2)
+  Space: O(n)
 */
 
 /* 
-  Add your pseudocode here
+  Create an object that keeps track of the numbers already seen
+  Iterate through each number in array
+  for current number, indentify if number matches for target (nummatch - array[i])
+  check if any key on our object is the matching number
+  If so, return true
+  Otherwise, add that number to object
+
+  If I reach the end of array, return false
+
+
+  hasTargetSum function
+  seenNumbers = {
+    1: true
+    2: true
+    3: true
+  }
 */
 
 /*
-  Add written explanation of your solution here
+  Create a function that takes in two arguments (array, target)
+  Iterate through the array adding two numbers together seeing if they equal the target sum
+  For example [1, 2, 3, 4, 5] can have a target of 7 because 2 + 5 = 7
+  If the two numbers added together equal the target sum return true
+  return false
 */
 
 // You can run `node index.js` to view these console logs
